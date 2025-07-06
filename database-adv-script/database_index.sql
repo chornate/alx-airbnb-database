3a9ad15989dd70f31446eb02a5c6127cd0f27cfc
chornate <155074527+chornate@users.noreply.github.com>
2025-07-06 19:15:13 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,9 +1,8 @@
-CREATE INDEX idx_users_user_id ON users(user_id);
-CREATE INDEX idx_users_email ON users(email);
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 123;
 
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
 CREATE INDEX idx_bookings_property_id ON bookings(property_id);
-CREATE INDEX idx_bookings_booking_date ON bookings(booking_date);
 
-CREATE INDEX idx_properties_property_id ON properties(property_id);
-CREATE INDEX idx_properties_city ON properties(city);
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 123;
