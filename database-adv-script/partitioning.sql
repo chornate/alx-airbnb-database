2ee443d91667cad34277e9144ccba1ae999e7ce2
chornate <155074527+chornate@users.noreply.github.com>
2025-07-06 19:34:51 +0300

    Update partitioning.sql

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -14,4 +14,6 @@ CREATE TABLE bookings_partitioned
     start_date DATE,
     end_date DATE,
     booking_date DATE
-) ON bookingDateRangePS(start_date);
+) 
+ON bookingDateRangePS(start_date)
+PARTITION BY RANGE (start_date);
