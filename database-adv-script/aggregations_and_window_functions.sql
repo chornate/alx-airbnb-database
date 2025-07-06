430c1732037f0e47e173322fb60fe8c1a13cb045
chornate <155074527+chornate@users.noreply.github.com>
2025-07-06 19:10:55 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -7,6 +7,7 @@ GROUP BY user_id;
 SELECT 
     property_id,
     COUNT(*) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_number_rank,
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS rank_rank
 FROM bookings
 GROUP BY property_id;
